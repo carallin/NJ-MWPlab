22a2588a8a798c8539578134ad96c3373a2373ca
carallin <carallin_t@163.com>
2017-02-09 21:09:55 +0800

    make rules for patents and papers

--- public/js/papers.js
+++ public/js/papers.js
@@ -1,9 +1,10 @@
 var papersYear = [2017,2016];
+//每一年的按照正序！！！！！
 var papers = [
   [{
     "id": 1,
     "name": "Microwave Photonic Filter With Two Independently Tunable Passbands Using a Phase Modulator and an Equivalent Phase-Shifted Fiber Bragg Grating",
-    "src": "",
+    "src": "../files/a-2017-1.pdf",
     "author": "GAO, J. ZHANG, X. CHEN, and J. YAO",
     "express": "IEEE Transactions on Microwave Theory"
   },
